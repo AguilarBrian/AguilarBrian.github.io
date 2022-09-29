7e2d99a6378b9921f86f65d90eedf009b296cb8c
AguilarBrian <brianisaiasaguilar@gmail.com>
2022-09-29 02:45:36 -0300

    update

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,123 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  "imagebaseurl": "https://github.com/AguilarBrian",
+  "name": "Brian Aguilar",
+  "role": "Full Stack Developer",
+  "linkedinId": "braiaguilar",
+  "skypeid": "braiaguilar",
+  "roleDescription": " With experience in web applications development. Eager to collaborate in a motivated team to build out-of-the-box solutions for clients. Skilled in developing problem solutions, disperse uncertainty, and manage team dynamism.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/braiaguilar",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "http://github.com/AguilarBrian",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "skype",
+      "url": "http://twitter.com/braiaguilar",
+      "className": "fa fa-twitter"
+    }
+  ],
+  "aboutme": "I am currently a Full Stack Developer at a startup called DOC24 and studying a degree in Systems Engineering at the Universidad Tecnologica Nacional. I have experience in JavaScript, Node, Express, React, Redux, PostgreSQL, MongoDB, HTML and CSS. I am a quick learner and I am always looking to improve my skills.",
+  "address": "Buenos Aires, Argentina",
+  "website": "brianisaiasaguilar@gmail.com",
+  "education": [
+    {
+      "UniversityName": "Universidad Tecnologica Nacional",
+      "specialization": "Systems Engineering",
+      "MonthOfPassing": "Estimating",
+      "YearOfPassing": "2028",
+    },
+    {
+      "UniversityName": "Universidad Nacional de La Plata",
+      "specialization": "Computer Science",
+      "MonthOfPassing": "Sept",
+      "YearOfPassing": "2022",
+    },
+    {
+      "UniversityName": "The University of Chicago",
+      "specialization": "Fintech",
+      "MonthOfPassing": "Apr",
+      "YearOfPassing": "2020",
+    },
+    {
+      "UniversityName": "Henry",
+      "specialization": "Full Stack Development - Coding Bootcamp",
+      "MonthOfPassing": "Jun",
+      "YearOfPassing": "2021",
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "DOC24",
+      "specialization": "Full Stack Developer",
+      "MonthOfLeaving": "-",
+      "YearOfLeaving": "Present",
+      "Achievements": "Built from start to finish the database, backend and frontend of a web app"
+    },
+    {
+      "CompanyName": "Treggo",
+      "specialization": "Full Stack Developer",
+      "MonthOfLeaving": "Feb",
+      "YearOfLeaving": "2022",
+      "Achievements": "Migrated the entire backend"
+    },
+    {
+      "CompanyName": "Henry",
+      "specialization": "Teaching Assistant - Full Stack Development",
+      "MonthOfLeaving": "May",
+      "YearOfLeaving": "2021",
+      "Achievements": "Taught 20+ students the fundamentals of Full Stack Development."
+    }
+  ],
+  "skillsDescription": "My skills",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "Nodejs"
+    },
+    {
+      "skillname": "Expressjs"
+    },
+    {
+      "skillname": "PostgreSQL"
+    },
+    {
+      "skillname": "MongoDB"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "DOC24 Telerehab",
+      "description": "Telerehabilitation app that uses TensorFlow.js to detect the user's movements.",
+      "imgurl": "images/portfolio/1.jpg",
+      "modal": "https://dtelerehabilitacion.doc24.com.ar/"
+    },
+    {
+      "name": "EATX",
+      "description": "Ecommerce app for an interplanetary restaurant.",
+      "imgurl": "images/portfolio/2.jpg",
+      "modal": "https://ec-webft11-g10.vercel.app/"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "Brian es una persona super original y proactivo! trabajamos juntos en el bootcamp Soy Henry donde desarrollamos un proyecto fullstack, este periodo fue genial! él tiene una gran capacidad analitica para abordar problemas y generar soluciones metódicas. Un gran desarrollador!",
+      "name": "Angel Zarate: Frontend Developer at Mercado Libre"
+    }
+  ]
+}
+
+export default resumeData
