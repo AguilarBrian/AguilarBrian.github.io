e3327a940df0162243670791cbd2cbc69bd7f7f5
AguilarBrian <brianisaiasaguilar@gmail.com>
2022-10-28 10:44:26 -0300

    Profile pic

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default class About extends Component {
 
             <div className="three columns">
 
-               <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
+               <img className="profile-pic"  src="images/profilepic.png" alt="" />
 
             </div>
 
@@ -16,8 +16,8 @@ export default class About extends Component {
 
                <h2>About Me</h2>
                <p>
-               I am currently a Full Stack Developer at a startup called DOC24 and studying a degree in Systems Engineering at the Universidad Tecnologica Nacional (UTN).<br></br><br></br>
-               I have experience in JavaScript, TypeScript, Node, Express, React, Redux, Material UI, Bootstrap, Next.js, PostgreSQL, MongoDB, HTML and CSS.<br></br><br></br>
+               I am currently a Full Stack Developer at a startup called DOC24 and studying a degree in Systems Engineering at the Universidad Tecnologica Nacional (UTN). Member of Python Argentina.<br></br><br></br>
+               I have experience in Python, JavaScript, TypeScript, Node, Express, React, Redux, Material UI, Bootstrap, Next.js, PostgreSQL, MongoDB, HTML and CSS.<br></br><br></br>
                I am a quick learner and I am always looking to improve my skills.<br></br><br></br>
                In my free time I am a coffee lover, a cat lover and I like to work on personal projects, participate in hackathons and attend meetings and conferences.
                </p>
