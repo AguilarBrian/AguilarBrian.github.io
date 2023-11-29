cc24aceda06a980d6ad109973b368f76848d3c9f
AguilarBrian <brianisaiasaguilar@gmail.com>
2023-11-29 17:22:49 -0300

    About me & layout details

--- src/components/About.js
+++ src/components/About.js
@@ -16,10 +16,8 @@ export default class About extends Component {
 
                <h2>About Me</h2>
                <p>
-               I am currently a Full Stack Developer at a startup called DOC24 and studying a degree in Systems Engineering at the Universidad Tecnologica Nacional (UTN). Member of Python Argentina.<br></br><br></br>
-               I have experience in Python, JavaScript, TypeScript, Node, Express, React, Redux, Material UI, Bootstrap, Next.js, PostgreSQL, MongoDB, HTML and CSS.<br></br><br></br>
-               I am a quick learner and I am always looking to improve my skills.<br></br><br></br>
-               In my free time I am a coffee lover, a cat lover and I like to work on personal projects, participate in hackathons and attend meetings and conferences.
+               Experienced in web development and AI tools and libraries. Studying a Data Science, quick learner and always looking to improve my skills. Member of Python Argentina.<br></br><br></br>
+               In my free time I read, spend time with my cats, and learn languages..<br></br><br></br>
                </p>
 
                <div className="row">
